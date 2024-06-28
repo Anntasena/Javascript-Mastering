93c47cad59cbf013c3e05cec23c7ec59b4bbc35b
Anntasena <matlailsyahrin@gmail.com>
2024-06-28 16:12:11 +0700

    update

---
+++ 01-Javascript-Fundamentals/03-Values-and-Variables/script.js
@@ -0,0 +1,27 @@
+// Value & Variables
+
+let firstName = "Syahrin";
+console.log(firstName);
+
+// let 4year = 4 => ini cara deklarasi yang salah
+
+// Nama Variable harus dimulai dengan huruf, garis bawah (_), atau tanda dollar ($)
+// Contoh yg benar:
+let year4 = 4;
+let _year4 = 4;
+let $year4 = 4;
+
+// Nama variabel peka terhadap huruf besar dan kecil
+let myVariable = 10;
+let myvariable = 20;
+
+// Nama variabel tidak boleh menggunakan kata kunci yang sudah ada dalam JavaScript seperti let, class, return, function, dll.
+// let let = 5; // Salah
+// let function = 10; // Salah
+
+// Namakan variable sesuai nilai yang tercantum
+let myFirstJob = "Barista";
+let currentJob = "Student"
+// nama variable diatas lebih mudah dibaca
+let job1 = "Barista";
+let job2 = "Student"
