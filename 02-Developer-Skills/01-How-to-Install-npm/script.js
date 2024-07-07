136a833263c6542a0fa396a4f379826a7c05d36c
Anntasena <matlailsyahrin@gmail.com>
2024-07-07 21:32:57 +0700

    update

---
+++ 02-Developer-Skills/01-How-to-Install-npm/script.js
@@ -0,0 +1,7 @@
+// Remember, we're gonna use strict mode in all scripts now!
+"use strict";
+
+////////////////////////////////////////////////////
+//////////// Cara instal npm (node package manager)
+
+// npm intall (nama package) -g || -g artinya global
