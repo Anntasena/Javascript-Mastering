0deaf704aeb210e749facbe0c19667573d4cd96d
Anntasena <matlailsyahrin@gmail.com>
2024-07-08 14:45:53 +0700

    update

--- 02-Developer-Skills/05-How-to-Debugging/script.js
+++ 02-Developer-Skills/05-How-to-Debugging/script.js
@@ -10,3 +10,20 @@
         - Bugs adalah bagian normal dari pengembangan software (karna kita manusia dan manusia tempatnya salah :)
         - Faktanya setiap aplikasi kompleks mengandung bug, yang terkadang tidak ditemukan selama bertahun tahun yang dapat menyebabkan lubang di keamaanan atau masalah lainnya
 */
+
+// Prosses debugging
+/*
+    > 1. Menidentifikasi => sadar bahwa ada semacam bugs
+        > menemukan bugs bisa dengan:
+            - automatic testing software
+            - laporan penggunaan selama produksi (bugs yang ditemukan selama produksi adalah yang terburuk)
+            - mempertimbangankan dimana bugs itu sering terjadi
+    > 2. Mencari => mengisolasi dimana bug itu terjadi
+        - bisa menggunakan developer console
+    > 3. Memperbaiki => membetulkan bug
+        - mengganti solusi yang salah menjadi solusi baru yang benar
+    > 4. Mencegah => mencegah bug terjadi lagi di basis kode
+        - mencari projek yang mempunyai bug sama dalam kode yang sama
+            *sangat mustahil bahwa kita melakukan kesalahan yang sama
+        - menulis test menggunakan software testing
+*/
